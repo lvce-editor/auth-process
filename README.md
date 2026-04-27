05db58bfab3f359e51e8fafc589835556098bd79
Le Vivilet <levivilet@pm.me>
2026-04-27 23:49:02 +0200

    feature: add initial files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# auth-process
-Auth Process
+# File System Process
+
+File System Process
+
+## Gitpod
+
+[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/lvce-editor/file-system-process)
---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import * as config from '@lvce-editor/eslint-config'
+
+export default [
+  ...config.default,
+  ...config.recommendedNode,
+  {
+    files: ['**/*.ts'],
+    rules: {
+      'jest/no-restricted-jest-methods': 'off',
+      '@typescript-eslint/prefer-readonly-parameter-types': 'off',
+    },
+  },
+  {
+    files: ['**/Logger.ts'],
+    rules: {
+      'no-console': 'off',
+    },
+  },
+  {
+    rules: {
+      '@cspell/spellchecker': 'off',
+    },
+  },
+]
