59b6d12ca3f818b04088a8c5a0a7e1897549c990
Le Vivilet <levivilet@pm.me>
2026-04-27 23:51:17 +0200

    fix

--- packages/auth-process/test/EncodingType.test.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@jest/globals'
-import * as EncodingType from '../src/parts/EncodingType/EncodingType.ts'
-
-test('EncodingType.Utf8 has correct value', () => {
-  expect(EncodingType.Utf8).toBe('utf8')
-})
--- packages/auth-process/test/Env.test.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@jest/globals'
-import * as Env from '../src/parts/Env/Env.ts'
-
-test('getElectronRunAsNode returns environment variable', () => {
-  const result = Env.getElectronRunAsNode()
-  expect(typeof result).toBe('string')
-})
