07579deb851f847cbaa7322afe4438d3b81d3db0
Le Vivilet <levivilet@pm.me>
2026-04-28 08:56:05 +0200

    feat: add HandleElectronMessagePort functionality and tests

--- packages/auth-process/src/parts/CommandMap/CommandMap.ts
+++ packages/auth-process/src/parts/CommandMap/CommandMap.ts
@@ -1,6 +1,8 @@
+import * as HandleElectronMessagePort from '../HandleElectronMessagePort/HandleElectronMessagePort.ts'
 import * as OAuthServer from '../OAuthServer/OAuthServer.ts'
 
 export const commandMap = {
+  'HandleElectronMessagePort.handleElectronMessagePort': HandleElectronMessagePort.handleElectronMessagePort,
   'OAuthServer.create': OAuthServer.create,
   'OAuthServer.dispose': OAuthServer.dispose,
   'OAuthServer.getCode': OAuthServer.getCode,
